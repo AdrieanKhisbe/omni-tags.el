37e983ed49a171a6fd7b77ccbb48e975faf31c76
Adriean Khisbe <adriean.khisbe@live.fr>
2014-07-17 18:14:24 +0200

    renaming: omnitag: functions and face [ot:prefix]

--- mytag-face.el
+++ mytag-face.el
@@ -1,35 +1,37 @@
+;;; Specials faces for `omni-tags-mode'
+;;; `ot' is the namepace prefix
+
 ;; fichier temporaire pour la définition de mes faces perso:
 ;; http://www.gnu.org/software/emacs/manual/html_node/elisp/Defining-Faces.html#Defining-Faces
 ;; §TD:adddoc!! change default
 ;; Each of these symbols is both a face name, and a variable whose default value is the symbol itself.
 
-; §TD: add some doc, and inheritance! (might help to benefit from font look priority)
+;; §TD: add some doc, and `inheritance'! (might help to benefit from font look priority)
 
-(defvar  mytag-tagsymbol 'mytag-tagsymbol)
-(defface mytag-tagsymbol
+(defvar  ot:tag-symbol-face 'ot:tagsymbol-face)
+(defface ot:tag-symbol-face
   '((t (:weight bold :color "DeepSkyBlue2")))
   "Font Lock mode face used for begining of a tag."
-  :group 'font-lock-faces)
+  :group 'font-lock-faces) ;; §maybe: change group?
 
-(defvar  mytag-tagsymbols  'mytag-tagsymbols )
-(defface mytag-tagsymbols
+(defvar  ot:tag-symbols-face  'ot:tagsymbols-face )
+(defface ot:tag-symbols-face
   '((t (:weight bold :color "DeepSkyBlue3")))    nil    :group 'font-lock-faces) ;doc to create
 
-(defvar  mytag-name 'mytag-name	)
-(defface mytag-name
+(defvar  ot:name-face 'ot:name-face	)
+(defface ot:name-face
   '((t (:weight bold :color "SteelBlue1"   )))    nil    :group 'font-lock-faces) ;doc to create
 
-(defvar  mytag-details 'mytag-details	)
-(defface mytag-details
+(defvar  ot:details-face 'ot:details-face	)
+(defface ot:details-face
   '((t (:slant italic :color "LightSteelBlue1" )))    nil    :group 'font-lock-faces) ;doc to create
 
-(defvar  mytag-ponctuation 'mytag-ponctuation)
-(defface mytag-ponctuation
+(defvar  ot:ponctuation-face 'ot:ponctuation-face)
+(defface ot:ponctuation-face
   '((t (:weight bold :color "red3")))    nil    :group 'font-lock-faces) ;doc to create
 
-(defvar  mytag-separation 'mytag-separation)
-(defface mytag-separation
+(defvar  ot:separation-face 'ot:separation-face)
+(defface ot:separation-face
   '((t (:weight bold :color "OrangeRed1" )))    nil    :group 'font-lock-faces) ;doc to create
-
 
 (provide 'mytag-face)
--- mytag-mode.el
+++ mytag-mode.el
@@ -1,7 +1,7 @@
 ;;; By Adriean Khisbe
 ;;; §building: for nom, just import
 
-(require 'pcre2el)
+(require 'pcre2el) ;§maybe: only use in development for starting performance issue
 (require 'mypersonaltagface)
 
 ;; §doc: HOW: default: at begining: set:ecrase, othervalues: at the end
@@ -10,41 +10,40 @@
 ;; §TF! ?? what _ symbolize in regexp? match ;?
 ;; §TD: make list of different keyword, more highlighted! for instance: TD TF TI TM
 
-
 ;; §see: name?
-(defvar mt:primary-tag "§" "Tag primaire. (associé aux actions)")
-(defvar mt:secondary-tag "¤" "Tag secondaire. (associé aux descriptions)")
+(defvar ot:primary-tag "§" "Tag primaire. (associé aux actions)")
+(defvar ot:secondary-tag "¤" "Tag secondaire. (associé aux descriptions)")
 ;; todo: add   :type 'boolean  :group 'linum)
 
-(defvar mt:font-lock-mode t "Flag to use font lock face (until overiding is fix)")
-
+(defvar ot:font-lock-mode t "Flag to use font lock face (until overiding is fix)")
 
 ;; Factorisation des noms de font pour facilement switcher
-;; §fontlist: mytag-tagsymbol mytag-tagsymbols mytag-ponctuation mytag-separation mytag-name mytag-details
-(if (not mt:font-lock-mode)
-    (setq mt:fsymb mytag-tagsymbol
-	  mt:fsymbss mytag-tagsymbols
-	  mt:fponct mytag-ponctuation
-	  mt:fsep mytag-separation
-	  mt:fname mytag-name
-	  mt:fd mytag-details)
-  (setq mt:fsymb font-lock-keyword-face
-	mt:fsymbss font-lock-keyword-face
-	mt:fponct font-lock-warning-face
-	mt:fsep font-lock-warning-face
-	mt:fname font-lock-type-face
-	mt:fdet font-lock-comment-delimiter-face))
+;; §fontlist:(original) mytag-tagsymbol mytag-tagsymbols mytag-ponctuation mytag-separation mytag-name mytag-details
+
+(if (not ot:font-lock-mode)
+    (setq ot:fsymb ot:tag-symbol-face
+	  ot:fsymbss ot:tag-symbols-face
+	  ot:fponct ot:ponctuation-face
+	  ot:fsep ot:separation-face
+	  ot:fname ot:name-face
+	  ot:fd ot:details-face)
+  (setq ot:fsymb font-lock-keyword-face
+	ot:fsymbss font-lock-keyword-face
+	ot:fponct font-lock-warning-face
+	ot:fsep font-lock-warning-face
+	ot:fname font-lock-type-face
+	ot:fdet font-lock-comment-delimiter-face))
 ;; note: local variables, not exported
 ;; §todo: defface inheritance! (maybe fix priority of face)
 
-(defvar mt:tag-patterns nil "Ensemble des patterns à matcher")
+(defvar ot:tag-patterns nil "Ensemble des patterns à matcher")
 ;; §HERE : todo ;: créer variables spécifiques: single, composed, complex.
 ;; § voir comment late bind le tag
 
-;; (defvar mt:pattern-simple  "Pattern for single" )
-(defun mt:make-pattern (regexp)
+;; (defvar ot:pattern-simple  "Pattern for single" )
+(defun ot:make-pattern (regexp)
   "Create a pattern. Replace first %s with tag symbol, and convert pcre format to emacs regexp"
-  (rxt-pcre-to-elisp (format regexp (format "(%s+|%s+)" mt:primary-tag mt:secondary-tag)))
+  (rxt-pcre-to-elisp (format regexp (format "(%s+|%s+)" ot:primary-tag ot:secondary-tag)))
   )
 
 ;;; Keywords Definition:
@@ -55,21 +54,21 @@
 ;; use prepend to "override" comment face  §idea: make this behavior configurable
 ;; LAXMATCH: dont throw error if a sibexp is not matchd
 
-(defvar mt:tag-wonder-keyword
-  `(,(mt:make-pattern "%s([!?¿¡]+)");; §TODO: extract to var
-    (1 mt:fsymb t)
-    (2 mt:fponct t))
+(defvar ot:tag-wonder-keyword
+  `(,(ot:make-pattern "%s([!?¿¡]+)");; §TODO: extract to var
+    (1 ot:fsymb t)
+    (2 ot:fponct t))
   "wonder/expression tag")
 ;; §maybe; extract defintion in function to enable to reevalute it (after change pattern)
 
-(defvar mt:tag-detailed-keyword
-  `(,(mt:make-pattern "%s(['@\-_ [:alnum:]]+)(([:])([_,-;/[:alnum:]]+))*([:?!¡¿]+)?")
+(defvar ot:tag-detailed-keyword
+  `(,(ot:make-pattern "%s(['@\-_ [:alnum:]]+)(([:])([_,-;/[:alnum:]]+))*([:?!¡¿]+)?")
     ;; §tofix: combo a:b:c
-    (1 mt:fsymb t)
-    (2 mt:fname t)
-    (4 mt:fsep t "laxmatch")
-    (5 mt:fdet t "lax")
-    (6 mt:fponct t "laxmatch"))
+    (1 ot:fsymb t)
+    (2 ot:fname t)
+    (4 ot:fsep t "laxmatch")
+    (5 ot:fdet t "lax")
+    (6 ot:fponct t "laxmatch"))
   "Complex Tag §TD: repeat the same one without quotes")
 
 ;; New tags to create
@@ -78,28 +77,28 @@
 
 
 ;; §note: peut etre à supprimer
-(setq mt:tag-patterns
+(setq ot:tag-patterns
       `(
 	;; online si pas de sousexpr
-	( ,(mt:make-pattern "%s:\w*>")  . 'font-lock-warning-face) ; Inline, MArche en principe, pattern tofix
-	,mt:tag-detailed-keyword
-	,mt:tag-wonder-keyword
+	( ,(ot:make-pattern "%s:\w*>")  . 'font-lock-warning-face) ; Inline, MArche en principe, pattern tofix
+	,ot:tag-detailed-keyword
+	,ot:tag-wonder-keyword
 	))
 
   ;;; Coloration des tags des tags
-(defun mt:font-on ()
+(defun ot:font-on ()
   "adds font-lock for my personals §Tags"                                       ;
   (font-lock-add-keywords
    nil  ; §doc: Mode, if nil means that it's applied to current buffer. otherwise specify mode
-   mt:tag-patterns))
+   ot:tag-patterns))
 
-(defun mt:font-off ()
+(defun ot:font-off ()
   "Remove font-lock for my personals §Tags"                                       ;
-  (font-lock-add-keywords nil mt:tag-patterns))
+  (font-lock-add-keywords nil ot:tag-patterns))
 
   ;;; ¤* Utils fonctions
 ;; §todo: autoload
-(defun mt:occur-tags ()
+(defun ot:occur-tags ()
   "Call occur on My §tags"
   (interactive)
   (occur "§\\w+" )) ;; §TODO: use pattern!
@@ -108,14 +107,14 @@
 
 ;; §todo:AA!! fail gracely
 ;; cycle?
-(defun mt:next-tags ()
+(defun ot:next-tags ()
   "Go to next §tags"
   (interactive)
  (unless (search-forward-regexp "§\\w+" nil t)
    (message "No More Founds Tags!")))
 ;; §maybe: si ressaye, revient au début?
 
-(defun mt:previous-tags ()
+(defun ot:previous-tags ()
   "Go to prev §tags"
   (interactive)
  (unless (search-forward-regexp "§\\w+" nil t)
@@ -125,23 +124,24 @@
 ;; §todo: impr: message erreur
 ;; org opening of folding
 ;; §next: tagpuration?
-;; §idée: move dans `mt:default-config' ?
+;; §idée: move dans `ot:default-config' ?
 ;; §TODO: proposer dans la documentation un use!!
 
 ;;;###autoload
-(define-minor-mode mytag-mode
+(define-minor-mode omni-tags-mode
   "Colorize 'Personal tags' in the buffer."
   :lighter " §"
   :keymap (let ((map (make-sparse-keymap)))
-	    (define-key map (kbd "M-§") 'mt:next-tags)
-	    (define-key map (kbd "C-§") 'mt:previous-tags)
-	    (define-key map (kbd "C-M-§") 'mt:occur-tags)
+	    (define-key map (kbd "M-§") 'ot:next-tags)
+	    (define-key map (kbd "C-§") 'ot:previous-tags)
+	    (define-key map (kbd "C-M-§") 'ot:occur-tags)
             map)
-  (progn (if mytag-mode
-	     (mt:font-on)
-	   (mt:font-off))))
+  (progn (if omni-tags-mode
+	     (ot:font-on)
+	   (ot:font-off))))
 
-(add-hook 'org-mode-hook 'mytag-mode)
-(add-hook 'prog-mode-hook 'mytag-mode)
+;; §config:
+;; (add-hook 'org-mode-hook 'omni-tags-mode)
+;; (add-hook 'prog-mode-hook 'omni-tags-mode)
 
-(provide 'mytag-mode)
+(provide 'omni-tags)
