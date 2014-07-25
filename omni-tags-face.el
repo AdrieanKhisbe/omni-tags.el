8f355bd6e2baf963829fcdd2b8c33010489fab9a
Adriean Khisbe <adriean.khisbe@live.fr>
2014-07-25 21:19:52 +0200

    refactor:faces delete faces defvar

--- omni-tags-face.el
+++ omni-tags-face.el
@@ -8,62 +8,55 @@
 
 ;; §maybe: pass color as custom. (at least mine. haha.
 ;; §maybe: switch about face occurs here? (option require?)
-;; §maybe: speify mode according light/dark. and tty.
+;; §maybe: specify mode according light/dark. and tty.
 
 ;; docs:
-(setq doc:symbol "Face used for the tag symbol (leading character)"
-      doc:symbols "Face used for the consecutive tag symbols"
-      doc:name "Face used for the first keyword of the tag"
-      doc:separation "Face used for the separators inside a tag" ;§maybe: rename to separator
-      doc:ponctuation "Face used for trailing ponctuation"
-      doc:details "Face used for addition details inside a tag")
+
 ;; ¤maybe: add reference to the mode?
 ;; ¤why:warningfree variable? §maybe: pass to let?
 ;; ¤see: trailing dot?
 
 ;; §TODO: def group
 
-(defvar ot:tag-symbol-face 'ot:tag-symbol-face doc:symbol)
 (defface ot:tag-symbol-face
   '((t (:inherit font-lock-keyword-face
 		 :weight bold :foreground "DeepSkyBlue2")))
-  doc:symbol
+  "Face used for the tag symbol (leading character)"
   :group 'font-lock-faces) ;; §maybe: change group: use omni-tag one. (not that need of this inheritence anymore.-> maybe create support)
 
 (defvar  ot:tag-symbols-face 'ot:tag-symbols-face doc:symbols)
 (defface ot:tag-symbols-face
   '((t (:inherit font-lock-keyword-face
 		 :weight bold :foreground "DeepSkyBlue3")))
-  doc:symbols
+"Face used for the consecutive tag symbols"
   :group 'font-lock-faces)
 
 (defvar  ot:name-face 'ot:name-face doc:name)
 (defface ot:name-face
   '((t (:inherit font-lock-type-face ;§maybe reverse then
 		 :weight bold :foreground "SteelBlue1"   )))
-  doc:name
+"Face used for the first keyword of the tag"
   :group 'font-lock-faces)
 
 (defvar  ot:details-face 'ot:details-face doc:details)
 (defface ot:details-face
   '((t (:inherit font-lock-comment-delimiter-face ;§maybe change
 		 :slant italic :foreground "LightSteelBlue1" )))
-  doc:details
+"Face used for addition details inside a tag"
   :group 'font-lock-faces)
 
-(defvar ot:ponctuation-face 'ot:ponctuation-face doc:ponctuation)
 (defvar  ot:ponctuation-face 'ot:ponctuation-face)
 (defface ot:ponctuation-face
   '((t (:inherit font-lock-warning-face
 		   :weight bold :foreground "red3")))
-  doc:ponctuation
+"Face used for the separators inside a tag"
   :group 'font-lock-faces)
 
 (defvar  ot:separation-face 'ot:separation-face doc:separation)
 (defface ot:separation-face
   '((t (:inherit font-lock-warning-face
 		 :weight bold :foreground "OrangeRed1" )))
-  doc:separation
+  "Face used for trailing ponctuation"
   :group 'font-lock-faces)
 
 (provide 'omni-tags-face)
