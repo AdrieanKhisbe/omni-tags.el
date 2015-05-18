cfcb07428d30ef9465e7745e138704de248e1694
Adriean Khisbe <adriean.khisbe@live.fr>
2015-05-18 21:15:02 +0200

    add scaffold ecukes

---
+++ features/support/env.el
@@ -0,0 +1,32 @@
+(require 'f)
+
+(defvar omni-tags-support-path
+  (f-dirname load-file-name))
+
+(defvar omni-tags-features-path
+  (f-parent omni-tags-support-path))
+
+(defvar omni-tags-root-path
+  (f-parent omni-tags-features-path))
+
+(add-to-list 'load-path omni-tags-root-path)
+
+(require 'omni-tags)
+(require 'espuds)
+(require 'ert)
+
+(Setup
+ ;; Before anything has run
+ )
+
+(Before
+ ;; Before each scenario is run
+ )
+
+(After
+ ;; After each scenario is run
+ )
+
+(Teardown
+ ;; After when everything has been run
+ )
